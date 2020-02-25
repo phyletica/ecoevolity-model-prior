eaaa106f5b7e5d40ef24200b93b106df99c1a08b
Jamie R. Oaks <joaks1@gmail.com>
2020-02-25 15:29:13 -0600

    Adding scripts to generate dummy data files.
    Adding scripts to generate dummy biallelic data files. These dummy data
    are not used, but determine the size of the data sets simcoevolity will
    simulate.

---
+++ scripts/create-dummy-data-files.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+nspecies=2
+ngenomes=4
+nchars=500000
+
+data_dir="../data"
+mkdir -p "$data_dir"
+
+# Generate 500k dummy alignments
+i=1
+while [ "$i" -lt 11 ]
+do
+    prefix="c${i}sp"
+    comp_str="0${i}"
+    if [ ${#i} -gt 1 ]
+    then
+        comp_str="$i"
+    fi
+    outfile="${data_dir}/comp${comp_str}-${nspecies}species-${ngenomes}genomes-${nchars}chars.txt"
+    ./generate-dummy-data-file.py \
+        --nspecies "$nspecies" \
+        --ngenomes "$ngenomes" \
+        --ncharacters "$nchars" \
+        --prefix "$prefix" \
+        > "$outfile"
+    i=`expr $i + 1`
+done
