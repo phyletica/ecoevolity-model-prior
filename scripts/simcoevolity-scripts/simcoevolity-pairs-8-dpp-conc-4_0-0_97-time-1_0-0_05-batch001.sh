3b65ba47977aa00182881fa0937996b1983dd34a
Jamie R. Oaks <joaks1@gmail.com>
2020-02-25 23:50:35 -0600

    Updating simcoevolity qsub scripts.
    Having them follow up running simcoevolity with calls to Python scripts
    to set up configs and qsub scripts.

--- scripts/simcoevolity-scripts/simcoevolity-pairs-8-dpp-conc-4_0-0_97-time-1_0-0_05-batch001.sh
+++ scripts/simcoevolity-scripts/simcoevolity-pairs-8-dpp-conc-4_0-0_97-time-1_0-0_05-batch001.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 if [ -z "$ECOEVOLITY_MODEL_PRIOR_PROJECT_DIR" ]
 then
@@ -14,22 +14,20 @@ fi
 
 if [ -n "$PBS_JOBNAME" ]
 then
-    source ${PBS_O_HOME}/.bash_profile
     cd $PBS_O_WORKDIR
     source "${ECOEVOLITY_MODEL_PRIOR_PROJECT_DIR}/modules-to-load.sh" 
 fi
 
-path_to_this_script="$0"
-name_of_this_script="$(basename "$path_to_this_script")"
-name_after_prefix="${name_of_this_script#*simcoevolity-}"
-config_name="${name_after_prefix%-batch*}"
+config_name="pairs-8-dpp-conc-4_0-0_97-time-1_0-0_05"
 config_path="../../ecoevolity-configs/${config_name}.yml"
 output_dir="../../ecoevolity-simulations/${config_name}/batch001"
 rng_seed=1
 number_of_reps=10
 
 exe_path="${ECOEVOLITY_MODEL_PRIOR_BIN_DIR}/simcoevolity"
+config_set_up_script_path="${ECOEVOLITY_MODEL_PRIOR_PROJECT_DIR}/scripts/set-up-configs-for-simulated-data.py"
+qsub_set_up_script_path="${ECOEVOLITY_MODEL_PRIOR_PROJECT_DIR}/scripts/set-up-ecoevolity-qsubs.py"
 
 mkdir -p "$output_dir"
 
-$exe_path --seed="$rng_seed" -n "$number_of_reps" -o "$output_dir" "$config_path"
+$exe_path --seed="$rng_seed" -n "$number_of_reps" -o "$output_dir" "$config_path" && $config_set_up_script "$output_dir" && $qsub_set_up_script_path "$output_dir"
