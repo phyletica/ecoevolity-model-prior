ac60db3e6a924f6c90f4dfde6e8662476930bc03
Jamie R. Oaks <joaks1@gmail.com>
2020-02-25 23:48:32 -0600

    Adding Python scripts.
    Adding project_util.py to serve as the master utility file for all
    Python scripts in the project.
    
    Adding script to convert configs generated by simcoevolity into
    configs that will be used to analyze the simulated data.
    
    Adding script to generate qsub files to run ecoevolity on each simulated
    data set.

---
+++ scripts/project_util.py
@@ -0,0 +1,142 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import re
+import argparse
+
+# Project paths
+SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+PROJECT_DIR = os.path.dirname(SCRIPT_DIR)
+SIM_DIR = os.path.join(PROJECT_DIR, 'ecoevolity-simulations')
+CONFIG_DIR = os.path.join(PROJECT_DIR, 'ecoevolity-configs')
+BIN_DIR = os.path.join(PROJECT_DIR, 'bin')
+RESULTS_DIR = os.path.join(PROJECT_DIR, 'results')
+PLOT_DIR = os.path.join(RESULTS_DIR, 'plots')
+
+# Project regular expressions
+SIMCOEVOLITY_CONFIG_NAME_PATTERN_STR = (
+        r"simcoevolity-sim-(?P<sim_num>\d+)-config.yml")
+SIMCOEVOLITY_CONFIG_NAME_PATTERN = re.compile(
+        r"^" + SIMCOEVOLITY_CONFIG_NAME_PATTERN_STR + r"$")
+
+SIM_CONFIG_TO_USE_PATTERN_STR = (
+        r"(?P<var_only>var-only-)?(?P<config_name>pairs-\S+)-sim-(?P<sim_num>\d+)-config.yml")
+SIM_CONFIG_TO_USE_PATTERN = re.compile(
+        r"^" + SIM_CONFIG_TO_USE_PATTERN_STR + r"$")
+
+
+PBS_HEADER =  """#! /bin/bash
+
+if [ -z "$ECOEVOLITY_MODEL_PRIOR_PROJECT_DIR" ]
+then
+    echo "ERROR: ECOEVOLITY_MODEL_PRIOR_PROJECT_DIR is not set"
+    exit 1
+fi
+
+if [ -z "$ECOEVOLITY_MODEL_PRIOR_BIN_DIR" ]
+then
+    echo "ERROR: ECOEVOLITY_MODEL_PRIOR_BIN_DIR is not set"
+    exit 1
+fi
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    cd $PBS_O_WORKDIR
+    source "${ECOEVOLITY_MODEL_PRIOR_PROJECT_DIR}/modules-to-load.sh" 
+fi
+
+"""
+
+
+def file_path_iter(directories, regex_pattern):
+    for directory in directories:
+        for dir_path, dir_names, file_names in os.walk(directory):
+            for f_name in file_names:
+                if regex_pattern.match(f_name):
+                    path = os.path.join(dir_path, f_name)
+                    yield path
+
+def simcoevolity_config_iter(sim_directories = None):
+    if sim_directories is None:
+        sim_directories = [SIM_DIR]
+    for path in file_path_iter(sim_directories, SIMCOEVOLITY_CONFIG_NAME_PATTERN):
+        yield path
+
+def sim_configs_to_use_iter(sim_directories = None):
+    if sim_directories is None:
+        sim_directories = [SIM_DIR]
+    for path in file_path_iter(sim_directories, SIM_CONFIG_TO_USE_PATTERN):
+        yield path
+
+# Utility functions for argparse
+def arg_is_path(path):
+    try:
+        if not os.path.exists(path):
+            raise
+    except:
+        msg = 'path {0!r} does not exist'.format(path)
+        raise argparse.ArgumentTypeError(msg)
+    return path
+
+def arg_is_file(path):
+    try:
+        if not os.path.isfile(path):
+            raise
+    except:
+        msg = '{0!r} is not a file'.format(path)
+        raise argparse.ArgumentTypeError(msg)
+    return path
+
+def arg_is_dir(path):
+    try:
+        if not os.path.isdir(path):
+            raise
+    except:
+        msg = '{0!r} is not a directory'.format(path)
+        raise argparse.ArgumentTypeError(msg)
+    return path
+
+def arg_is_nonnegative_int(i):
+    try:
+        if int(i) < 0:
+            raise
+    except:
+        msg = '{0!r} is not a non-negative integer'.format(i)
+        raise argparse.ArgumentTypeError(msg)
+    return int(i)
+
+def arg_is_positive_int(i):
+    try:
+        if int(i) < 1:
+            raise
+    except:
+        msg = '{0!r} is not a positive integer'.format(i)
+        raise argparse.ArgumentTypeError(msg)
+    return int(i)
+
+def arg_is_positive_float(i):
+    try:
+        if float(i) <= 0.0:
+            raise
+    except:
+        msg = '{0!r} is not a positive real number'.format(i)
+        raise argparse.ArgumentTypeError(msg)
+    return float(i)
+
+def arg_is_nonnegative_float(i):
+    try:
+        if float(i) < 0.0:
+            raise
+    except:
+        msg = '{0!r} is not a non-negative real number'.format(i)
+        raise argparse.ArgumentTypeError(msg)
+    return float(i)
+
+
+def main():
+    sys.stdout.write("{0}".format(PROJECT_DIR))
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/set-up-ecoevolity-qsubs.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import argparse
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+import project_util
+
+def write_qsub(config_path,
+        run_number = 1,
+        relax_missing_sites = False):
+    qsub_prefix = os.path.splitext(config_path)[0]
+    qsub_path = "{0}-run-{1}-qsub.sh".format(qsub_prefix, run_number)
+    if os.path.exists(qsub_path):
+        return
+    config_file = os.path.basename(config_path)
+    stdout_path = "run-{0}-{1}.out".format(run_number, config_file)
+    seed = run_number
+    assert(not os.path.exists(qsub_path))
+    with open(qsub_path, 'w') as out:
+        out.write(project_util.PBS_HEADER)
+        out.write("exe_path=\"${ECOEVOLITY_MODEL_PRIOR_BIN_DIR}/simcoevolity\"\n\n")
+        if relax_missing_sites:
+            out.write("$exe_path --seed {0} --prefix run-{1}- --relax-constant-sites --relax-missing-sites {2} 1>{3} 2>&1\n".format(
+                    seed,
+                    run_number,
+                    config_file,
+                    stdout_path))
+        else:
+            out.write("$exe_path --seed {0} --prefix run-{1}- --relax-constant-sites {2} 1>{3} 2>&1\n".format(
+                    seed,
+                    run_number,
+                    config_file,
+                    stdout_path))
+
+
+def main_cli():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('sim_dirs',
+            metavar = 'SIMCOEVOLITY-OUTPUT-DIR',
+            nargs = '+',
+            type = project_util.arg_is_dir,
+            help = ('Paths to directories with simcoevolity output files.'))
+    parser.add_argument('--number-of-runs',
+            action = 'store',
+            type = int,
+            default = 4,
+            help = 'Number of qsubs to generate per config (Default: 4).')
+
+    args = parser.parse_args()
+
+    for config_path in project_util.sim_configs_to_use_iter(args.sim_dirs):
+        for i in range(args.number_of_runs):
+            write_qsub(config_path = config_path,
+                    run_number = i + 1,
+                    relax_missing_sites = False)
+    
+
+if __name__ == "__main__":
+    main_cli()
