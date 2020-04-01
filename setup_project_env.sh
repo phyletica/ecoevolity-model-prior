a95b768f94e1600b847cef841aecb51d9372febc
Jamie R. Oaks <joaks1@gmail.com>
2020-03-31 21:41:04 -0500

    Updating setup_project_env.sh script.
    Removing the creation of hidden file in the home directory with project
    path info. This is not longer used (sourced) by any scripts, because all
    paths are now relative.

--- setup_project_env.sh
+++ setup_project_env.sh
@@ -7,16 +7,6 @@ ecoevolity_commit="932c358ce"
 # Get path to directory of this script
 project_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-bashrc_path="${HOME}/.bashrc"
-bash_project_config_path="${HOME}/.bash_ecoevolity_model_prior_project"
-
-# Create a hidden config file in the home directory with useful path variables
-# This will overwrite the config file if it exists to make sure it is current
-echo "Creating shell config file for the project in the home directory... "
-echo "export ECOEVOLITY_MODEL_PRIOR_PROJECT_DIR=\"${project_dir}\"" > "$bash_project_config_path"
-echo "export ECOEVOLITY_MODEL_PRIOR_BIN_DIR=\"${project_dir}/bin\"" >> "$bash_project_config_path"
-echo "    Created \"$bash_project_config_path\""
-
 # Load modules
 echo "Loading modules..."
 source ./modules-to-load.sh >/dev/null 2>&1 || echo "    No modules loaded"
