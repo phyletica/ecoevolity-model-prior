4fe9736794ae491d54fabb1c9e972cf3e79b8d84
Jamie R. Oaks <joaks1@gmail.com>
2020-04-10 22:10:30 -0500

    Updating Sphinx documentation conf.py.
    Enabling the use of Markdown (along with restructured text).

--- docs-source/source/conf.py
+++ docs-source/source/conf.py
@@ -45,7 +45,7 @@ extensions = [
         'sphinx.ext.imgmath',
         'sphinx.ext.githubpages',
         'sphinxcontrib.bibtex',
-        # 'recommonmark',
+        'recommonmark',
         ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -54,12 +54,11 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
-# source_suffix = {
-#         '.rst': 'restructeredtext',
-#         '.txt': 'markdown',
-#         '.md': 'markdown',
-#         }
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # The master toctree document.
 master_doc = 'index'
