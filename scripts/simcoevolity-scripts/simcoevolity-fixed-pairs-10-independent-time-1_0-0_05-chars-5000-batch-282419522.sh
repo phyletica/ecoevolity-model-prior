2824a36583767e27ce7bb556fbae74a6ce94d85c
Kerry Cobb <cobbkerry@gmail.com>
2020-06-17 12:16:48 -0500

    Adding scripts and simulation results for batches 239392945, 282419522, 922214979, 634009014,and 261452082
    Scripts will run simcoevolity to generate a batch of 10
    simulated datasets each for 4 different sizes of datasets.
    
    Adding gzipped, tab-delimited files. Each file summarizes the results of
    ecoevolity analyses of 10 simcoevolity simulation replicates. Adding
    these
    files for simulations of 4 different dataset sizes, each analysed with 3
    models, with and without constant characters.

---
+++ scripts/simcoevolity-scripts/simcoevolity-fixed-pairs-10-independent-time-1_0-0_05-chars-5000-batch-282419522.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+set -e
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    if [ -f "${PBS_O_HOME}/.bashrc" ]
+    then
+        source "${PBS_O_HOME}/.bashrc"
+    fi
+    cd $PBS_O_WORKDIR
+fi
+
+rng_seed=282419522
+number_of_reps=10
+
+config_name="fixed-pairs-10-independent-time-1_0-0_05-chars-5000"
+config_path="../../ecoevolity-configs/${config_name}.yml"
+output_dir="../../ecoevolity-simulations/${config_name}/batch-${rng_seed}"
+
+project_dir="../.."
+exe_path="${project_dir}/bin/simcoevolity"
+config_set_up_script_path="${project_dir}/scripts/set_up_configs_for_simulated_data.py"
+qsub_set_up_script_path="${project_dir}/scripts/set_up_ecoevolity_qsubs.py"
+
+if [ ! -f "$exe_path" ]
+then
+    echo "ERROR: File '$exe_path' does not exist."
+    echo "       You probably need to run the project setup script."
+    exit 1
+fi
+
+source "${project_dir}/modules-to-load.sh" >/dev/null 2>&1 || echo "    No modules loaded"
+
+if [ -n "$(command -v conda)" ]
+then
+    eval "$(conda shell.bash hook)"
+    conda activate ecoevolity-model-prior-project
+fi
+
+mkdir -p "$output_dir"
+
+$exe_path --seed="$rng_seed" -n "$number_of_reps" -o "$output_dir" "$config_path" && $config_set_up_script_path "$output_dir" && $qsub_set_up_script_path "$output_dir"
